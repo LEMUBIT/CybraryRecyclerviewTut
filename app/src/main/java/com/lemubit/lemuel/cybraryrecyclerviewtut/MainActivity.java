92c3bc05f826dd48e29d84b64929be4f823f1dbd
Lemuel Ogbunude <lemuelcco@gmail.com>
2019-08-01 21:35:27 +0100

    Initial commit

---
+++ app/src/main/java/com/lemubit/lemuel/cybraryrecyclerviewtut/MainActivity.java
@@ -0,0 +1,35 @@
+package com.lemubit.lemuel.cybraryrecyclerviewtut;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerview);
+
+        String[] languages = {"Java", "Python", "C++", "Ruby", "Javascript", "C#", "C",
+                "Kotlin", "Swift", "Objective C", "Ruby", "Scala", "Clojure", "Rust", "PHP", "Go", "Groovy"};
+
+
+        LanguageAdapter languageAdapter = new LanguageAdapter(languages);
+
+        recyclerView.setAdapter(languageAdapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
+
+
+    }
+}
